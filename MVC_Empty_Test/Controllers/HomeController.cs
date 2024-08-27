818919e0f43224d7db9018bf0851f1d21b475645
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-08-27 18:58:45 +0300

    Initial commit

---
+++ MVC_Empty_Test/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Empty_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ContentResult result = new ContentResult();
+            result.Content = "Hello From Home !";
+            return result;
+        }
+
+        public IActionResult About()
+        {
+            return RedirectToRoute (new { controller = "Home", action = "Index" });
+        }
+    }
+}
---
+++ MVC_Empty_Test/Program.cs
@@ -0,0 +1,64 @@
+namespace MVC_Empty_Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            //app.MapGet("/", () => "Hello World!");
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            //app.MapGet("/", async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.MapGet("/products", async context =>
+            {
+                await context.Response.WriteAsync("Products Page Content");
+            });
+
+            app.MapGet("/product/{id:int}", async context =>
+            {
+                if (int.TryParse(context.Request.RouteValues["id"]?.ToString(), out int id))
+                {
+                    await context.Response.WriteAsync($"Product {id} Details Page Content");
+                }
+                else
+                {
+                    await context.Response.WriteAsync("Invalid id");
+                }
+            });
+
+            app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}",
+            defaults: new { controller = "Home", action = "Index" }
+            );
+
+            app.MapControllerRoute(
+            name: "about-us",
+            pattern: "about-us",
+            defaults: new { controller = "Home", action = "About" }
+            );
+
+            // page not found route handler
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404)
+                {
+                    await context.Response.WriteAsync("Page not found");
+                }
+            });
+            app.Run();
+        }
+    }
+}
